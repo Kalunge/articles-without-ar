783eefa07c6855a0f37aaed933eb16df6a22bb7d
Titus Muthomi <muthomititus+github@gmail.com>
2023-02-20 09:46:08 +0300

    Add author and name method

---
+++ author.rb
@@ -0,0 +1,19 @@
+# Author
+# Author#initialize(name)
+# An author is initialized with a name, as a string.
+# A name cannot be changed after it is initialized.
+# Author#name
+# Returns the name of the author
+
+class Author
+  # An author is initialized with a name, as a string.
+  # A name cannot be changed after it is initialized.
+  def initialize(name)
+    @name = name
+  end
+
+  # Returns the name of the author
+  def name
+    @name
+  end
+end
